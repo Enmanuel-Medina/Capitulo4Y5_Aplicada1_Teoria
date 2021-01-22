ecc5012e67d63cac323df6eab0f5ef069c52ad9c
Jose Enmanuel Jaquez Medina <jose_jaquez1@ucne.edu.do>
2021-01-22 13:37:20 -0700

    Add project files.

---
+++ CApitulo4y5/Capitulo4y5/Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CApitulo4y5.Capitulo4y5
+{
+    class Factorial
+    {
+        public int num { get; set; }
+
+        public Factorial() { }
+        public int factorial(int num)
+        {
+            if (num == 0 || num == 1)
+                return num;
+            return num * factorial(num - 1);
+        }
+
+        public void calcularFactorial()
+        {
+            Console.Write("Digite el numero: ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("El factorial de " + num + " es: " + factorial(num));
+        }
+    }
+}
---
+++ CApitulo4y5/Capitulo4y5/FormatoFechaYHora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CApitulo4y5.Capitulo4y5
+{
+    class FormatoFechaYHora
+    {
+        private string formato;
+
+        public FormatoFechaYHora() { }
+
+        public void correr()
+        {
+            formato = String.Format("Fecha:{0: h:m tt   d / M / yyy}", DateTime.Now);
+            Console.WriteLine(formato);
+        }
+    }
+}
